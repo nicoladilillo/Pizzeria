42be52a824c5988475698e6bb5f7541077d3cf39
Nicola Dilillo <nicoladilillo98@gmail.com>
2016-05-28 20:26:19 +0200

    Fix signup

--- signup.php
+++ signup.php
@@ -1,36 +1,27 @@
 <?php
-  $username=$_POST['user'];
-  $password=$_POST['password'];
-  $conferma=$_POST['confermapassword'];
-  $nome=$_POST['nome'];
 
-  $idmiofile = fopen("utenti.txt", "a+");
+$username=$_POST['user'];
+$password=$_POST['password'];                                                                   $conferma=$_POST['confermapassword'];                                                                   $nome=$_POST['nome'];                                                                                                                               $idmiofile = fopen("utenti.txt", "a+"); 
+                                                                                                        
+if (!$idmiofile) {
+ echo "Impossibile iscriversi al sito!";  
+ }                                                                   
+else {
+  $trovate = false; 
+  while(!feof($idmiofile)) {  
+    $letta=fgets($idmiofile); 
+    $dati=explode(";;",$letta);
+    if($dati[0]==$username)  $trovate = true;  
+  } 
+}
 
-  if (!$idmiofile) {
-    echo "Impossibile iscriversi al sito!";
-  }
-  else {
-    $trovate = false;
-    while(!feof($idmiofile)) {
-      $letta=fgets($idmiofile);
-      $dati=explode(";;",$letta);
-      if($dati[0]==$username)
-        $trovate = true;
-  }
+if ($trovate == true) 
+  echo "Username gia' esistente";   
+else if ($password != $conferma) 
+  echo "La password non corrisponde"; 
+else {
+ $record = ($username . ";;" . $password . ";;" . $nome . "\n");                                                                         fwrite($idmiofile, $record);
+ header("location: index.php");
+} 
 
-  echo "ciao";
-
-  if ($trovate == true) {
-    echo "Username gia' esistente";
-  }
-  else if ($password != $conferma) {
-    echo "La password non corrisponde";
-  }
-  else {
-    $record = ($username . ";;" . $password . ";;" . $nome . "\n");
-    fwrite($idmiofile, $record);
-  }
-
-  echo "Registrazione eseguita con successo";
-
-?>
+?>                                                                 
